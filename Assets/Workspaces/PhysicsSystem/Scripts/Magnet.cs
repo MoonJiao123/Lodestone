b109b063d84448a6aeb961fadfa0f2c92d6640f7
AndrewMJordan <andrew.m.jordan@hotmail.com>
2019-11-02 18:28:16 -0700

    Implements magnet architecture

---
+++ Assets/Workspaces/PhysicsSystem/Scripts/Magnet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace PhysicsSystem {
+
+	public class Magnet : MonoBehaviour {
+		[SerializeField]
+		private MagnetRaycaster raycaster;
+		[SerializeField]
+		private MagnetAffector affector;
+		[SerializeField]
+		private MagnetMediator mediator;
+
+		public MagnetRaycaster Raycaster => raycaster;
+		public MagnetAffector Affector => affector;
+		public MagnetMediator Mediator => mediator;
+	}
+}
---
+++ Assets/Workspaces/PhysicsSystem/Scripts/MagnetRaycaster.cs
@@ -0,0 +1,32 @@
+﻿using Andtech;
+using UnityEngine;
+
+namespace PhysicsSystem {
+
+	public class MagnetRaycaster : MonoBehaviour {
+		public bool HasTarget { get; private set; }
+		public RaycastHit HitInfo { get; private set; }
+		public Ray Ray { get; private set; }
+
+		[SerializeField]
+		private Transform actionPoint;
+		[SerializeField]
+		private LayerMask collisionMask;
+		[SerializeField]
+		private float maxDistance;
+
+		#region MONOBEHAVIOUR
+		protected virtual void Update() {
+			Ray ray = new Ray(actionPoint.position, actionPoint.forward);
+			Ray = ray;
+			HasTarget = Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance, collisionMask);
+			HitInfo = hitInfo;
+
+			if (HasTarget)
+				Debug.DrawLine(ray.origin, hitInfo.point, Color.green.Alpha(0.25F));
+			else
+				Debug.DrawRay(ray.origin, ray.GetPoint(maxDistance), Color.red.Alpha(0.25F));
+		}
+		#endregion
+	}
+}
